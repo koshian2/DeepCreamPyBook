3bd4a9c321e4a246ef8e0ca5fa58038c097b0315
こしあん <saltcoffeehorse@gmail.com>
2019-03-26 01:35:46 +0900

    Update svhn.py

--- PConv/libs/svhn.py
+++ PConv/libs/svhn.py
@@ -2,6 +2,7 @@ import torchvision
 from scipy.io import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
+from mask import MaskGenerator
 
 def load_svhn():
     # データをダウンロードするためにTorchVisionを使う
@@ -23,5 +24,21 @@ def plot_svhn():
         ax.axis("off")
     plt.show()
 
-if __name__ == "__main__":
-    plot_svhn()
+def plot_svhn_with_masks():
+    _, X_test = load_svhn()
+    maskgen = MaskGenerator(64, 64, rand_seed=114514)
+    plt.subplots_adjust(hspace=0.02, wspace=0.02, top=0.98, bottom=0.02, left=0.02, right=0.98)
+    for i in range(3):
+        img = X_test[i].repeat(2, axis=0).repeat(2, axis=1)
+        ax = plt.subplot(3,3,3*i+1)
+        ax.imshow(img)
+        ax.axis("off")
+        mask = maskgen.sample()
+        ax = plt.subplot(3,3,3*i+2)
+        ax.imshow(mask*255)
+        ax.axis("off")
+        joined = mask * img
+        ax = plt.subplot(3,3,3*i+3)
+        ax.imshow(joined)
+        ax.axis("off")
+    plt.show()
